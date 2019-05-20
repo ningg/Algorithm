c3433795561462b1edf0cccb2229fe28a6c430a8
Ning Guo <guoning.gn@gmail.com>
2019-05-20 12:40:58 +0800

    feature: 二叉树遍历

--- src/main/java/top/ningg/algo/tree/TraverseTree.java
+++ src/main/java/top/ningg/algo/tree/TraverseTree.java
@@ -40,6 +40,53 @@ public class TraverseTree {
     }
 
     /**
+     * 遍历：后续（非递归，循环方式）
+     *
+     * @param root 二叉树根节点
+     * @param result 遍历输出的结果
+     */
+    public static void postOrderTraverseLoop(TreeNode root, List<TreeNode> result) {
+        Stack<TreeNode> stack = new Stack<>();
+
+        // 基本逻辑：非空「左子树」循环入栈，顶点不出栈，右子树有效则继续「左子树循环入栈」，若子节点全部出栈则顶部节点出栈
+        // 1. 初始化：记录当前节点，为 root 节点
+        // 2. 循环逻辑：
+        // a. 「当前节点」非 null，则，入栈，并将「左子节点」设置为「当前节点」
+        // b. 如果「当前节点」为 null，则，查询顶部节点（不弹出），判断其「右子节点」是否为 null or 为「上次输出」的节点，若满足，则，弹出「顶部节点」并输出
+        // c. 如果「当前节点」为 null，且顶部节点（不弹出），其「右子节点」不为 null，且「未被输出」，则，将其作为「当前节点」，进入循环逻辑
+        // 3. 终止条件：「栈」为空，且，「当前节点」也为空
+
+        if (null == root) {
+            return;
+        }
+
+        TreeNode currNode = root;
+        TreeNode lastRecordNode = null;
+
+        while (!stack.isEmpty() || currNode != null) {
+            // a. 「当前节点」不为 null，则，入栈，并以「左子节点」作为当前节点，继续迭代
+            if (null != currNode) {
+                stack.push(currNode);
+                currNode = currNode.left;
+            } else {
+                // b. 当前节点为 null，则，查询顶部节点
+                TreeNode topNode = stack.peek();
+
+                if (null != topNode.right && lastRecordNode != topNode.right) {
+                    // 1. 「顶部节点」可以「右子节点」进行递归，则继续递归
+                    currNode = topNode.right;
+                } else {
+                    // 2. 「顶部节点」满足「出栈条件」，则出栈访问
+                    topNode = stack.pop();
+                    lastRecordNode = topNode;
+                    result.add(topNode);
+                }
+
+            }
+        }
+    }
+
+    /**
      * 遍历：中序（非递归，循环方式）
      *
      * @param root 二叉树根节点
@@ -48,16 +95,40 @@ public class TraverseTree {
     public static void inOrderTraverseLoop(TreeNode root, List<TreeNode> result) {
         Stack<TreeNode> stack = new Stack<>();
 
+        // 基本逻辑：非空「左子树」循环入栈，顶点出栈，「右子树」继续「左子树循环入栈」
+        // 1. 初始化：记录当前节点，为 root 节点
+        // 2. 循环逻辑：
+        // a. 「当前节点」非 null，则入栈，并，将「左子节点」设置为「当前节点」
+        // b. 如果「当前节点」为 null，则，弹出一个节点，记录到结果中
+        // c. 以「弹出的节点」的「右子节点」为基准，设置为「当前节点」
+        // 3. 终止条件：「栈」为空，且，「当前节点」也为空
+
+        if (null == root) {
+            return;
+        }
+
+        TreeNode currNode = root;
+
+        while (!stack.isEmpty() || null != currNode) {
+            if (null != currNode) {
+                stack.push(currNode);
+                currNode = currNode.left;
+            } else {
+                TreeNode validNode = stack.pop();
+                result.add(validNode);
+                currNode = validNode.right;
+            }
+        }
 
     }
 
     /**
      * 遍历：前序（非递归，循环方式）
      *
-     * TODO：增加示意图描述，done
+     * Note：有独立的示意图
      *
      * @param root 二叉树根节点
-     * @param result 中序遍历的结果
+     * @param result 前序遍历的结果
      */
     public static void preOrderTraverseLoop(TreeNode root, List<TreeNode> result) {
         Stack<TreeNode> stack = new Stack<>();
