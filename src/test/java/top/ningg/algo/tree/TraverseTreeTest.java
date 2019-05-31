18fd890a62e76eda508b9fe341c6970afa6aedb2
Ning Guo <guoning.gn@gmail.com>
2019-05-31 21:42:14 +0800

    feature: 补充二叉树遍历 code

--- src/test/java/top/ningg/algo/tree/TraverseTreeTest.java
+++ src/test/java/top/ningg/algo/tree/TraverseTreeTest.java
@@ -41,11 +41,70 @@ public class TraverseTreeTest {
 
         while (!stack.isEmpty()) {
             TreeNode currNode = stack.pop();
+            result.add(currNode);
+
             if (null != currNode.right) {
                 stack.push(currNode.right);
             }
             if (null != currNode.left) {
                 stack.push(currNode.left);
+            }
+        }
+    }
+
+    // 二叉树：循环方式，实现遍历，中序遍历
+    public static void inOrderTraverseLoop(TreeNode root, List<TreeNode> result) {
+        if (null == root) {
+            return;
+        }
+
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode currNode = root;
+
+        while (!stack.isEmpty() || currNode != null) {
+            if (null != currNode) {
+                stack.push(currNode);
+                currNode = currNode.left;
+            } else {
+                TreeNode popNode = stack.pop();
+                result.add(popNode);
+
+                currNode = popNode.right;
+            }
+        }
+
+    }
+
+
+    // 二叉树：循环方式，实现遍历，后序遍历
+    public static void postOrderTraverseLoop(TreeNode root, List<TreeNode> result) {
+        if (null == root) {
+            return;
+        }
+
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode currNode = root;
+        TreeNode lastRecordNode = null;
+
+        while (!stack.isEmpty() || null != currNode) {
+            if (null != currNode) {
+                stack.push(currNode);
+                currNode = currNode.left;
+            } else {
+                TreeNode topNode = stack.peek();
+                if (null == topNode.right) {
+                    topNode = stack.pop();
+                    result.add(topNode);
+                    lastRecordNode = topNode;
+                } else {
+                    if (lastRecordNode == topNode.right) {
+                        topNode = stack.pop();
+                        result.add(topNode);
+                        lastRecordNode = topNode;
+                    } else {
+                        currNode = topNode.right;
+                    }
+                }
             }
         }
     }
